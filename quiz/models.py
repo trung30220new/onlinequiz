8b1357afaef08ebf8c35af0b00723a338a8df159
trung30220new <trung30220@gmail.com>
2025-01-12 14:44:09 +0700

    Update models.py

--- quiz/models.py
+++ quiz/models.py
@@ -18,22 +18,24 @@ class Question(models.Model):
     option4=models.CharField(max_length=200)
     cat=(('Option1','Option1'),('Option2','Option2'),('Option3','Option3'),('Option4','Option4'))
     answer=models.CharField(max_length=200,choices=cat)
+    # is_true_false = models.BooleanField(default=False)  # New True/False field
     def __str__(self):
         return "ques: " + str(self.id)
 
-# class Exam(models.Model):
-#     course=models.ForeignKey(Course,on_delete=models.CASCADE)
-#     ques=models.JSONField()
-#     code=models.CharField(max_length=200)
-#     start=models.DateTimeField()
-#     duration=models.PositiveIntegerField()
-#     def __str__(self):
-#         return 'exam: ' + str(self.id) + ' ' + str(self.start)
+class Exam(models.Model):
+    course=models.ForeignKey(Course,on_delete=models.CASCADE)
+    ques=models.JSONField()
+    exam_code=models.CharField(max_length=200)
+    start=models.DateTimeField(null=True)
+    duration_minutes=models.PositiveIntegerField()
+    number_of_ques=models.PositiveIntegerField(default=10)
+    def __str__(self):
+        return 'exam: ' + str(self.id) + ' ' + str(self.start)
 
 class Result(models.Model):
     student = models.ForeignKey(Student,on_delete=models.CASCADE)
-    exam = models.ForeignKey(Course,on_delete=models.CASCADE)
-    # exam = models.ForeignKey(Exam,on_delete=models.CASCADE)
+    # exam = models.ForeignKey(Course,on_delete=models.CASCADE)
+    exam = models.ForeignKey(Exam,on_delete=models.CASCADE)
     marks = models.PositiveIntegerField()
     date = models.DateTimeField(auto_now=True)
     def __str__(self):
